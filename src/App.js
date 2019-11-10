e21e05bc3940d68e561732ca1c716b9e659ca02a
pycckuu <is.markelov@gmail.com>
2019-11-10 01:26:23 -0500

    Transaction crafting and submission

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import * as React from 'react'
 import { ethers } from 'ethers'
 
 import WalletNotify from './components/WalletNotify'
+import WN from 'wallet-notify'
 
 const NETWORK = 'goerli'
 
-async function submitTx(to, notification) {
+async function submitTx(to, imageInput, messageInput, actionInput) {
+
+  const notification = {
+    t: messageInput,
+    tu: imageInput,
+    au: actionInput
+  }
   console.log('Version', ethers.version)
 
   const provider = new ethers.providers.Web3Provider(
@@ -15,25 +22,10 @@ async function submitTx(to, notification) {
   const gasPrice = await provider.getGasPrice()
   console.log('Gas Price:', gasPrice.toNumber())
 
-  const wallet = provider.getSigner()
+  const txHash = await WN.send({ to, notification, web3: window.web3, gasPrice, gasLimit: 210000 })
 
-  const balance = await wallet.getBalance()
-  console.log('Balance:', ethers.utils.formatEther(balance))
-
-  const note = JSON.stringify(notification)
-  console.log({ note })
-
-  const data = ethers.utils.toUtf8Bytes('!!' + note)
-  console.log('Tx Data:', data.toString())
-
-  const tx = await wallet.sendTransaction({
-    gasLimit: 210000,
-    gasPrice,
-    to,
-    data,
-  })
-  console.log(`Sent TX: https://${NETWORK}.etherscan.io/tx/${tx.hash}`)
-  console.log('TX Data:', tx.data)
+  console.log(`Sent TX: https://${NETWORK}.etherscan.io/tx/${txHash}`)
+  console.log('TX Data:', txHash)
 }
 
 export default function App() {
@@ -43,14 +35,7 @@ export default function App() {
     messageInput,
     actionInput,
   }) => {
-    console.log({
-      addressInput,
-      imageInput,
-      messageInput,
-      actionInput,
-    })
-    debugger
-
+    submitTx(addressInput, imageInput, messageInput, actionInput)
     // const notification = {
     //   t: this.state.notificationInput,
     //   u: this.state.urlInput,
