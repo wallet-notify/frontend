7d0d4103b33c43ff021528ad89fd5fa41b8d82b5
amsul <reach@amsul.ca>
2019-11-10 04:11:17 -0500

    Update the build
    Signed-off-by: amsul <reach@amsul.ca>

---
+++ docs/build/precache-manifest.eca7e1ac6575d1223e2dc9cc07e5b0bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed35485592dc6c997ecbe380ecb3af68",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "ea703b6b7d786f5d9489",
+    "url": "/frontend/static/css/main.be64c91e.chunk.css"
+  },
+  {
+    "revision": "da7ca58d62cd85fd9281",
+    "url": "/frontend/static/js/2.91f81441.chunk.js"
+  },
+  {
+    "revision": "ea703b6b7d786f5d9489",
+    "url": "/frontend/static/js/main.e8adc6e1.chunk.js"
+  },
+  {
+    "revision": "6a3115af67a299ee1482",
+    "url": "/frontend/static/js/runtime-main.be5c238e.js"
+  },
+  {
+    "revision": "dd09fbb7fb4cd5086fe7215ac2a34d64",
+    "url": "/frontend/static/media/logo.dd09fbb7.svg"
+  },
+  {
+    "revision": "3f1583f71c17e371c85c48d96321eab6",
+    "url": "/frontend/static/media/plus-large.3f1583f7.svg"
+  },
+  {
+    "revision": "bcfde67856cda5d8dc8a3cec1ad74603",
+    "url": "/frontend/static/media/plus-small.bcfde678.svg"
+  }
+]);
