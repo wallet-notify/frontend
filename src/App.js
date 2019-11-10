e05cf4b529ce5160faf06c03ecdf94ed4e413ce2
pycckuu <is.markelov@gmail.com>
2019-11-10 03:43:40 -0500

    reverted  changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,52 @@ import { ethers } from 'ethers'
 import WalletNotify from './components/WalletNotify'
 import WN from 'wallet-notify'
 
+const EthCrypto = require('eth-crypto')
+
 const NETWORK = 'goerli'
+
+async function submitTxOld(to, imageInput, messageInput, actionInput) {
+
+
+  const notification = {
+    t: messageInput,
+    tu: imageInput,
+    au: actionInput
+  }
+
+  console.log('Version', ethers.version)
+
+  const provider = new ethers.providers.Web3Provider(
+    window.web3.currentProvider,
+  )
+
+  const gasPrice = await provider.getGasPrice()
+  console.log('Gas Price:', gasPrice.toNumber())
+
+  const wallet = provider.getSigner()
+
+  const balance = await wallet.getBalance()
+  console.log('Balance:', ethers.utils.formatEther(balance))
+
+  const note = JSON.stringify(notification)
+  console.log({ note })
+
+  const data = window.web3.toHex('!!' + EthCrypto.cipher.stringify(note))
+
+  console.log('Tx Data:', data.toString())
+
+  const tx = await wallet.sendTransaction({
+    gasLimit: 210000,
+    gasPrice,
+    to,
+    data,
+  })
+
+  console.log(`Sent TX: https://${NETWORK}.etherscan.io/tx/${tx.hash}`)
+  console.log('TX Data:', tx.data)
+}
+
+
 
 async function submitTx(to, imageInput, messageInput, actionInput) {
 
@@ -35,7 +80,7 @@ export default function App() {
     messageInput,
     actionInput,
   }) => {
-    submitTx(addressInput, imageInput, messageInput, actionInput)
+    submitTxOld(addressInput, imageInput, messageInput, actionInput)
     // const notification = {
     //   t: this.state.notificationInput,
     //   u: this.state.urlInput,
